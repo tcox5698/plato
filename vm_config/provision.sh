3683bc2c5aca2a9a2bf2f36630f0a3c7b333667d
tcox5698 <tcox56_98@yahoo.com>
2014-11-24 22:38:54 -0600

    made it work

--- vm_config/provision.sh
+++ vm_config/provision.sh
@@ -1,5 +1,9 @@
 #!/bin/bash   
 
+echo 'config timezone START'
+echo "America/Chicago" | sudo tee /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+echo 'config timezone FINISH'
 
 echo 'config repositories START'
 if [ -e .repos_configged ]
@@ -16,7 +20,7 @@ if [ -e .packages_installed ]
   then echo 'packages installed - skipping'
   else
     sudo apt-get update
-    sudo apt-get --assume-yes install xorg gnome-core gnome-system-tools gnome-app-install default-jre git postgresql curl zsh google-chrome-stable  > /dev/null
+    sudo apt-get --assume-yes install xorg gnome-core gnome-system-tools gnome-app-install default-jre git postgresql curl zsh google-chrome-stable libpq-dev nodejs npm > /dev/null
     touch .packages_installed
 fi
 echo 'install packages FINISH'
@@ -25,6 +29,7 @@ echo 'install ruby and rails START'
 if [ -e .ruby_rails_installed ]
   then echo 'ruby and rails installed - skipping'
   else  
+    sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
     \curl -sSL https://get.rvm.io | bash -s stable --rails
     touch .ruby_rails_installed
 fi
@@ -35,6 +40,21 @@ if [ -e .oh_my_zsh_installed ]
   then echo 'oh-my-zsh installed - skipping'
   else
     curl -L http://install.ohmyz.sh | sh
+    chsh -s $(which zsh)
     touch .oh_my_zsh_installed
 fi
 echo 'install oh-my-zsh FINISH'
+
+#echo 'install RubyMine START'
+#if [ -e .rubymine_installed ]
+#  then echo 'rubymine installed - skipping'
+#  else
+#    wget 'http://download.jetbrains.com/ruby/RubyMine-7.0.tar.gz'
+#    tar xfz RubyMine-7.0.tar.gz
+#    touch .rubymine_installed
+#fi 
+#echo 'install RubyMine FINISH'
+
+echo 'get permissions to rvm/rubies START'
+sudo chmod -R a=rwx /usr/local/rvm/rubies
+echo 'get permissions to rvm/rubies FINISH'
