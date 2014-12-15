44a8b361ea7844c41bf3159ab30c894449757c8b
tcox5698 <tcox56_98@yahoo.com>
2014-12-14 21:20:06 -0600

    stuff

--- vm_config/provision.sh
+++ vm_config/provision.sh
@@ -25,36 +25,45 @@ if [ -e .packages_installed ]
 fi
 echo 'install packages FINISH'
 
+echo 'install prezto START'
+if [ -e .prezto_installed ]
+  then echo 'prezto installed - skipping'
+  else
+    su -l vagrant /vagrant/vm_config/install_prezto.sh
+    touch .prezto_installed
+fi
+echo 'install prezto FINISH'
+
 echo 'install ruby and rails START'
 if [ -e .ruby_rails_installed ]
   then echo 'ruby and rails installed - skipping'
   else  
     sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-    \curl -sSL https://get.rvm.io | bash -s stable --rails
+    su -l vagrant /vagrant/vm_config/install_ruby_rails.sh
     touch .ruby_rails_installed
 fi
 echo 'install ruby and rails FINISH'
 
-echo 'install oh-my-zsh START'
-if [ -e .oh_my_zsh_installed ]
-  then echo 'oh-my-zsh installed - skipping'
-  else
-    su -l vagrant /vagrant/vm_config/install_oh_my_zsh.sh
-    touch .oh_my_zsh_installed
-fi
-echo 'install oh-my-zsh FINISH'
+#echo 'install oh-my-zsh START'
+#if [ -e .oh_my_zsh_installed ]
+#  then echo 'oh-my-zsh installed - skipping'
+#  else
+#    su -l vagrant /vagrant/vm_config/install_oh_my_zsh.sh
+#    touch .oh_my_zsh_installed
+#fi
+#echo 'install oh-my-zsh FINISH'
 
-echo 'install RubyMine START'
-if [ -e .rubymine_installed ]
-  then echo 'rubymine installed - skipping'
-  else
-    cp /vagrant/RubyMine-7.0.tar.gz ~/
-    #wget 'http://download.jetbrains.com/ruby/RubyMine-7.0.tar.gz'
-    tar xfz RubyMine-7.0.tar.gz
-    touch .rubymine_installed
-fi 
-echo 'install RubyMine FINISH'
+#echo 'install RubyMine START'
+#if [ -e .rubymine_installed ]
+#  then echo 'rubymine installed - skipping'
+#  else
+#    cp /vagrant/RubyMine-7.0.tar.gz ~/
+#    #wget 'http://download.jetbrains.com/ruby/RubyMine-7.0.tar.gz'
+#    tar xfz RubyMine-7.0.tar.gz
+#    touch .rubymine_installed
+#fi
+#echo 'install RubyMine FINISH'
 
-echo 'get permissions to rvm/rubies START'
-sudo chmod -R a=rwx /usr/local/rvm/rubies
-echo 'get permissions to rvm/rubies FINISH'
+#echo 'get permissions to rvm/rubies START'
+#sudo chmod -R a=rwx /usr/local/rvm/rubies
+#echo 'get permissions to rvm/rubies FINISH'
