91e00449419aaeac37cde47d514539e169d6438a
tcox5698 <tcox56_98@yahoo.com>
2014-12-15 22:38:38 -0600

    got zprezto working

--- vm_config/provision.sh
+++ vm_config/provision.sh
@@ -1,26 +1,19 @@
 #!/bin/bash   
 
+set -e
+
 echo 'config timezone START'
 echo "America/Chicago" | sudo tee /etc/timezone
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 echo 'config timezone FINISH'
 
-echo 'config repositories START'
-if [ -e .repos_configged ]
-  then echo 'repositories configured - skipping'
-  else
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-    touch .repos_configged
-fi
-echo 'config repositories FINISH'
 
 echo 'install packages START'
 if [ -e .packages_installed ]
   then echo 'packages installed - skipping'
   else
     sudo apt-get update
-    sudo apt-get --assume-yes install xorg gnome-core gnome-system-tools gnome-app-install default-jre git postgresql curl zsh google-chrome-stable libpq-dev nodejs npm > /dev/null
+    sudo apt-get --assume-yes install default-jre git postgresql curl zsh libpq-dev nodejs npm > /dev/null
     touch .packages_installed
 fi
 echo 'install packages FINISH'
@@ -29,20 +22,20 @@ echo 'install prezto START'
 if [ -e .prezto_installed ]
   then echo 'prezto installed - skipping'
   else
-    su -l vagrant /vagrant/vm_config/install_prezto.sh
+    su -c '/bin/zsh /vagrant/vm_config/install_prezto.sh' vagrant
     touch .prezto_installed
 fi
 echo 'install prezto FINISH'
 
-echo 'install ruby and rails START'
-if [ -e .ruby_rails_installed ]
-  then echo 'ruby and rails installed - skipping'
-  else  
-    sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-    su -l vagrant /vagrant/vm_config/install_ruby_rails.sh
-    touch .ruby_rails_installed
-fi
-echo 'install ruby and rails FINISH'
+#echo 'install ruby and rails START'
+#if [ -e .ruby_rails_installed ]
+#  then echo 'ruby and rails installed - skipping'
+#  else
+#    sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+#    su -l vagrant /vagrant/vm_config/install_ruby_rails.sh
+#    touch .ruby_rails_installed
+#fi
+#echo 'install ruby and rails FINISH'
 
 #echo 'install oh-my-zsh START'
 #if [ -e .oh_my_zsh_installed ]
@@ -52,17 +45,6 @@ echo 'install ruby and rails FINISH'
 #    touch .oh_my_zsh_installed
 #fi
 #echo 'install oh-my-zsh FINISH'
-
-#echo 'install RubyMine START'
-#if [ -e .rubymine_installed ]
-#  then echo 'rubymine installed - skipping'
-#  else
-#    cp /vagrant/RubyMine-7.0.tar.gz ~/
-#    #wget 'http://download.jetbrains.com/ruby/RubyMine-7.0.tar.gz'
-#    tar xfz RubyMine-7.0.tar.gz
-#    touch .rubymine_installed
-#fi
-#echo 'install RubyMine FINISH'
 
 #echo 'get permissions to rvm/rubies START'
 #sudo chmod -R a=rwx /usr/local/rvm/rubies
