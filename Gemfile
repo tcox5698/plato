5adb8113edbb6b38b3560d201f865259dc953b4d
tcox5698 <tcox56_98@yahoo.com>
2015-01-04 09:08:44 -0600

    tested idea model

--- Gemfile
+++ Gemfile
@@ -26,8 +26,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring', group: :development
 
-gem 'rspec-rails', groups: [:development, :test]
-gem 'rspec', groups: [:development, :test]
+gem 'rspec-rails', group: [:test]
+gem 'rspec', group: [:test]
+gem 'shoulda-matchers', group: [:test]
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Idea, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:description) }
+  it { is_expected.to respond_to(:profit_rating) }
+  it { is_expected.to respond_to(:passion_rating) }
+  it { is_expected.to respond_to(:skill_rating) }
+
+  it { is_expected.to validate_numericality_of(:profit_rating).only_integer.is_less_than(6).is_greater_than(0) }
+  it { is_expected.to validate_numericality_of(:passion_rating).only_integer.is_less_than(6).is_greater_than(0) }
+  it { is_expected.to validate_numericality_of(:skill_rating).only_integer.is_less_than(6).is_greater_than(0) }
+
+  it { is_expected.to validate_presence_of(:name) }
 end
