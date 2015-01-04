5811a5d5d951d5d4a0861245b0458f69e53725ba
Tom Cox <tcox56_98@yahoo.com>
2015-01-04 15:51:42 -0600

    added bootstrap formatting and datetime formatting

--- spec/views/ideas/index.html.erb_spec.rb
+++ spec/views/ideas/index.html.erb_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe "ideas/index", :type => :view do
     render
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     assert_select "tr>td", :text => "Description".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => 3.to_s, :count => 2
+    assert_select "tr>td:nth-of-type(4)", :text => 1.to_s, :count => 2
+    assert_select "tr>td:nth-of-type(5)", :text => 2.to_s, :count => 2
+    assert_select "tr>td:nth-of-type(6)", :text => 3.to_s, :count => 2
   end
 end
