164ee8ecf1318c27c3a3b264b32dc78deab1985a
tcox5698 <tcox56_98@yahoo.com>
2015-01-04 09:08:44 -0600

    tested idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,6 @@
 class Idea < ActiveRecord::Base
+  validates :name, presence: true
+  validates_numericality_of :passion_rating, :only_integer => true, :less_than => 6, :greater_than => 0
+  validates_numericality_of :skill_rating, :only_integer => true, :less_than => 6, :greater_than => 0
+  validates_numericality_of :profit_rating, :only_integer => true, :less_than => 6, :greater_than => 0
 end
