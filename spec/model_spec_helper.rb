32d282933dde717353c9a06030abc709949bd158
Tom Cox <tcox56_98@yahoo.com>
2015-05-24 09:27:59 -0500

    only connect for model specs if no existing connection

--- spec/model_spec_helper.rb
+++ spec/model_spec_helper.rb
@@ -3,14 +3,16 @@ require 'active_record'
 require 'bundler/psyched_yaml'
 require 'shoulda/matchers'
 connection_info = YAML.load_file("config/database.yml")["test"]
-# ActiveRecord::Base.establish_connection(connection_info)
 
-# RSpec.configure do |config|
-#   config.around do |example|
-#     ActiveRecord::Base.transaction do
-#       example.run
-#       raise ActiveRecord::Rollback
-#     end
-#   end
-# end
+unless ActiveRecord::Base.connected?
+  ActiveRecord::Base.establish_connection(connection_info)
 
+  RSpec.configure do |config|
+    config.around do |example|
+      ActiveRecord::Base.transaction do
+        example.run
+        raise ActiveRecord::Rollback
+      end
+    end
+  end
+end
