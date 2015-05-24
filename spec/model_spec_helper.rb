552a8c576bdf20264c297507939941202c0cf8cb
tcox5698 <tcox56_98@yahoo.com>
2015-05-24 09:17:27 -0500

    machine gunning it

--- spec/model_spec_helper.rb
+++ spec/model_spec_helper.rb
@@ -3,14 +3,14 @@ require 'active_record'
 require 'bundler/psyched_yaml'
 require 'shoulda/matchers'
 connection_info = YAML.load_file("config/database.yml")["test"]
-ActiveRecord::Base.establish_connection(connection_info)
+# ActiveRecord::Base.establish_connection(connection_info)
 
-RSpec.configure do |config|
-  config.around do |example|
-    ActiveRecord::Base.transaction do
-      example.run
-      raise ActiveRecord::Rollback
-    end
-  end
-end
+# RSpec.configure do |config|
+#   config.around do |example|
+#     ActiveRecord::Base.transaction do
+#       example.run
+#       raise ActiveRecord::Rollback
+#     end
+#   end
+# end
 
